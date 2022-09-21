e1e233f898f7b6ec8249cf7b38a740e4fd5a6d3c
Dmitry <suprun1212@gmail.com>
2022-09-21 11:59:06 +0300

    init

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Дмитрий Супрун on 21.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
