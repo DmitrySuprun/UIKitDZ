9df4edcaeea4dee367ccedbbb65c4ebdb0c89e9a
Dmitry <suprun1212@gmail.com>
2022-09-22 09:29:07 +0300

    Temp

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    let reversedLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 40, y: 40, width: 200, height: 40)
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    let startButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.setTitle("Begin", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.center = view.center
+        startButton.addTarget(self, action: #selector(startGame(paramTarget:)), for: .touchUpInside)
+    }
+    
+    @objc func startGame(paramTarget: UIButton) {
+        let getWordAlertController = UIAlertController(title: "New game", message: "Enter any word:", preferredStyle: .alert)
+        getWordAlertController.addTextField()
+        let okAlertAction = UIAlertAction(title: "Ok", style: .default)
+        getWordAlertController.addAction(okAlertAction)
     }
 }
